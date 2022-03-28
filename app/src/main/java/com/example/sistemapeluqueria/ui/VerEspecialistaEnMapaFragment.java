c00b4a9ddc35b35fdd9b19928fd013d0502df4e4
Javier <u20191e814@upc.edu.pe>
2022-03-28 00:39:53 -0500

    integracion de mi cuenta

---
+++ app/src/main/java/com/example/sistemapeluqueria/ui/VerEspecialistaEnMapaFragment.java
@@ -0,0 +1,24 @@
+package com.example.sistemapeluqueria.ui;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.sistemapeluqueria.R;
+
+
+public class VerEspecialistaEnMapaFragment extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_ver_especialista_en_mapa, container, false);
+    }
+}
