0d176a26e5d099d5edc1f2f0600d6ee21134fda0
Javier <u20191e814@upc.edu.pe>
2022-05-03 22:43:01 -0500

    Cambios 2

--- app/src/main/java/com/example/sistemapeluqueria/ui/test.java
+++ app/src/main/java/com/example/sistemapeluqueria/ui/test.java
@@ -10,21 +10,10 @@ import android.view.ViewGroup;
 
 import com.example.sistemapeluqueria.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link test#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class test extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public test() {
         // Required empty public constructor
@@ -42,8 +31,7 @@ public class test extends Fragment {
     public static test newInstance(String param1, String param2) {
         test fragment = new test();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+
         fragment.setArguments(args);
         return fragment;
     }
@@ -52,8 +40,7 @@ public class test extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+
         }
     }
 
